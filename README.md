96f411a109d916041d444c12560f1c787cd5ff46
Richard Lyon <richthegeek@gmail.com>
2017-06-24 22:04:38 +0100

    Rewrite to node >= 8.x

---
+++ README.md
@@ -0,0 +1,32 @@
+# promise-redis-ejs
+
+`promise-redis-ejs` is a modern (>= 8.x) fork of [promise-redis](https://github.com/maxbrieiev/promise-redis) which makes [node_redis](https://github.com/NodeRedis/node_redis) return Promises.
+
+It uses native Promise objects and `util.promisify` for promisifacation, but allows providing an alternative client for use with [fakeredis](https://github.com/hdachev/fakeredis)
+
+
+## Installation
+```bash
+npm install promise-redis
+```
+
+## Normal Usage
+```javascript
+const PromiseRedis = require('promise-redis-ejs');
+const Redis = PromiseRedis();
+const client = Redis.createClient();
+
+client.keys('*').then((keys) => {
+  console.log(keys);
+});
+```
+
+## Using FakeRedis for testing
+```javascript
+const FakeRedis = require('fakeredis');
+const PromiseRedis = require('promise-redis-ejs');
+const client = PromiseRedis(FakeRedis).createClient();
+
+client.keys('*').then((keys) => {
+  console.log(keys); // empty array :)
+})```
--- index.js
+++ index.js
@@ -1,61 +1,22 @@
-'use strict';
+const redis = require('redis');
+const list = require('redis-commands').list;
+const promisify = require('util').promisify;
 
-var redis = require('redis'),
-    redisCmds = require('redis-commands').list;
+module.exports = (target = redis) => {
+  const multi = target.Multi.prototype;
+  const client = target.RedisClient.prototype;
 
-function createCb(resolve, reject) {
-    return function (err, value) {
-        if (err !== null) {
-            reject(err);
-        } else {
-            resolve(value);
-        }
-    };
-}
-
-module.exports = function (promiseFactory) {
-    var mlproto = redis.Multi.prototype,
-        clproto = redis.RedisClient.prototype;
-
-    if (!promiseFactory) {
-        promiseFactory = function (resolver) {
-            return new Promise(resolver);
-        };
-    }
-
-    function promisify(f) {
-        return function () {
-            var args = Array.prototype.slice.call(arguments),
-                that = this;
-            if (typeof args[args.length - 1] === 'function') {
-                // Okay. Someone supplied a callback. Most likely some internal
-                // node-redis call (ready probe etc.). Oh, as a result of
-                // supporting internal callback-style calls, one can now use
-                // promise-redis as a dropin replacement for node-redis.
-                f.apply(this, args);
-            } else {
-                return promiseFactory(function (resolve, reject) {
-                    args.push(createCb(resolve, reject));
-                    f.apply(that, args);
-                });
-            }
-        };
-    }
-
-    redisCmds.forEach(function (fullCommand) {
-        var cmd = fullCommand.split(' ')[0];
-
-        if (cmd !== 'multi') {
-            clproto[cmd] = promisify(clproto[cmd]);
-            clproto[cmd.toUpperCase()] = clproto[cmd];
-        }
-
-    });
+  list.filter((cmd) => {
+    return cmd !== 'multi' && 'function' === typeof client[cmd];
+  }).forEach((cmd) => {
+    client[cmd] = promisify(client[cmd]);
+    client[cmd.toUpperCase()] = client[cmd];
+  });
 
     // For Multi only `exec` command returns promise.
-    mlproto.exec_transaction = promisify(mlproto.exec_transaction);
-    mlproto.exec = mlproto.exec_transaction;
-    mlproto.EXEC = mlproto.exec;
+  multi.exec_transaction = promisify(multi.exec_transaction);
+  multi.exec = multi.exec_transaction;
+  multi.EXEC = multi.exec;
 
-    return redis;
+  return target;
 };
